a3b0dc64c3d58e9dbe447e4d836cd4d4086d2dd9
Rizvan Karimov <karimovrizvan84@gmail.com>
2023-12-25 10:42:41 +0100

    added Model and used in project,changed emp name and added neew description with model

--- src/main/java/com/rizvankarimov/spring/mvc/MyController.java
+++ src/main/java/com/rizvankarimov/spring/mvc/MyController.java
@@ -1,7 +1,12 @@
 package com.rizvankarimov.spring.mvc;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 public class MyController {
@@ -17,8 +22,20 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(){
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmpDetails(){
+    public String showEmpDetails(@NotNull HttpServletRequest request, Model model){String empName = request.getParameter("employeeName");
+         empName = "Mr." + empName;
+         model.addAttribute("nameAttribute", empName);
+
+         model.addAttribute("description", "- is java developer");
+
+
+
         return "show-emp-details-view";
     }
 }
